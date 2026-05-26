9a2e8425662496c205c859bd2b86e62f729a9763
Charis Dousis <xaris3434@gmail.com>
2026-05-26 20:12:44 +0300

    feat: add class and functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import FunctionalComponent from "./compenents/FunctionalComponent.tsx";
+import ClassComponent from "./compenents/ClassComponent.tsx";
+import ArrowFunctionalComponent from "./compenents/ArrowFunctionalComponent.tsx";
 
 function App() {
 
   return (
     <>
+      <FunctionalComponent />
+      <ClassComponent />
+      <ArrowFunctionalComponent />
     </>
   )
 }
