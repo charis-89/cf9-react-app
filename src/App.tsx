900bc0b88844214270c66d1a5c3cf5c690bcd6ea
Charis Dousis <xaris3434@gmail.com>
2026-06-01 12:54:26 +0300

    feat: add Header, Footer and Layout components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 //import ClassComponent from "./compenents/LessonTwo/ClassComponent.tsx";
 //import ArrowFunctionalComponent from "./compenents/LessonTwo/ArrowFunctionalComponent.tsx";
 import ArrowFunctionalComponentWithProps from "./compenents/LessonThree/ArrowFunctionalComponentWithProps.tsx";
+import Layout from "./compenents/Layout.tsx";
 
 // type PropsA = {
 //   title: string,
@@ -55,23 +56,32 @@ function App() {
       {/*<FunctionalComponent />*/}
       {/*<ClassComponent />*/}
       {/*<ArrowFunctionalComponent />*/}
+        {/*<h2 className="cf-text" id="id"></h2> /!*  h2.cf-text#id  *!/*/}
+
+        {/*Lorem ipsum dolor sit amet.*/}
+        {/*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, recusandae.*/}
+        {/*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, aspernatur cumque dolor eius, fuga fugiat in inventore laborum maiores minus odit praesentium qui ullam voluptatem voluptatum? Delectus harum iste quaerat!*/}
+
+        {/*/!*lorem  *!/*/}
+        {/*/!*lorem10*!/*/}
+        {/*/!*lorem100 + tab  *!/*/}
 
       {/* LESSON THREE */}
-      <ArrowFunctionalComponentWithProps title="Heading 1" />
-      <ArrowFunctionalComponentWithProps
-          title="Second Title"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, consequuntur doloremque enim expedita molestias mollitia natus nesciunt quae quasi quia.
-" />
+{/*      <ArrowFunctionalComponentWithProps title="Heading 1" />*/}
+{/*      <ArrowFunctionalComponentWithProps*/}
+{/*          title="Second Title"*/}
+{/*          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, consequuntur doloremque enim expedita molestias mollitia natus nesciunt quae quasi quia.*/}
+{/*" />*/}
 
-      {/*<h2 className="cf-text" id="id"></h2> /!*  h2.cf-text#id  *!/*/}
+      <Layout>
+          <ArrowFunctionalComponentWithProps
+              title="Second Title"
+              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, consequuntur doloremque enim expedita molestias mollitia natus nesciunt quae quasi quia."
+          />
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, itaque?</p>
+      </Layout>
 
-      {/*Lorem ipsum dolor sit amet.*/}
-      {/*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, recusandae.*/}
-      {/*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, aspernatur cumque dolor eius, fuga fugiat in inventore laborum maiores minus odit praesentium qui ullam voluptatem voluptatum? Delectus harum iste quaerat!*/}
 
-      {/*/!*lorem  *!/*/}
-      {/*/!*lorem10*!/*/}
-      {/*/!*lorem100 + tab  *!/*/}
 
     </>
   )
---
+++ src/compenents/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                This is a Footer area!
+            </footer>
+
+        </>
+    )
+}
+export default Footer;
